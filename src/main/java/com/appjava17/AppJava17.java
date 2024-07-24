7cdca060a8e92ebef1669879a789be799f14dc5c
Pernyei Gábor <45963704+Narghil@users.noreply.github.com>
2024-07-24 21:19:53 +0200

    Initial commit

---
+++ src/main/java/com/appjava17/AppJava17.java
@@ -0,0 +1,15 @@
+package com.appjava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJava17 {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AppJava17.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/appjava17/AppJava17Tests.java
@@ -0,0 +1,13 @@
+package com.appjava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJava17Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
